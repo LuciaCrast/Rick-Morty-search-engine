a21470c20059d8f00b4639af331bbdba65b493b0
Lucia Cano <luciacanoras@gmail.com>
2021-08-06 19:18:52 +0200

    Clean code

--- src/service/GetDataFromApi.js
+++ src/service/GetDataFromApi.js
@@ -1,21 +1,3 @@
-// function GetDataFromApi() {
-//   const ENDPOINT = "https://rickandmortyapi.com/api/character";
-//   return fetch(ENDPOINT)
-//     .then((response) => response.json())
-//     .then((response) =>
-//       response.results.map((character) => {
-//         return {
-//           id: character.id,
-//           name: character.name,
-//           status: character.status,
-//           species: character.species,
-//           image: character.image,
-//           episode: character.episode.length,
-//           origin: character.origin.name,
-//         };
-//       })
-//     );
-// }
 function GetDataFromApiCharacter(params = {}) {
   const ENDPOINT = "https://rickandmortyapi.com/api/character";
   const searchParams = new URLSearchParams();
