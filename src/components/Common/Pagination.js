c76a8e14ffc9f286ea2808bf0aa405e6b507a4c7
Lucia Cano <luciacanoras@gmail.com>
2021-09-15 18:38:33 +0200

    Add style changes on pagination's buttons

--- src/components/Common/Pagination.js
+++ src/components/Common/Pagination.js
@@ -2,10 +2,14 @@ import "./pagination.scss";
 
 function Pagination({ onClickAfter, onClickPrevious, currentSite, pages }) {
   return (
-    <section>
-      <button onClick={onClickPrevious}>Página Anterior</button>
-      <button onClick={onClickAfter}>Página siguiente</button>
-      <p className="paragraph">
+    <section className="sectionPagination">
+      <button className="sectionPagination__button" onClick={onClickPrevious}>
+        Página Anterior
+      </button>
+      <button className="sectionPagination__button" onClick={onClickAfter}>
+        Página Siguiente
+      </button>
+      <p className="sectionPagination__paragraph">
         Estás en la página {currentSite} de {pages}
       </p>
     </section>
